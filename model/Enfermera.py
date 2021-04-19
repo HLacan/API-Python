0604f54d934178124c4c4af58867d0ff3fa846cb
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-04-19 14:45:01 -0600

    se agrego el modelo de enfermera

---
+++ model/Enfermera.py
@@ -0,0 +1,9 @@
+class Enfermera():
+    def __init__(self, nombre, apellido, fecha, sexo, usuario, password, telefono):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.fecha = fecha
+        self.sexo = sexo
+        self.usuario = usuario
+        self.password = password
+        self.telefono = telefono
