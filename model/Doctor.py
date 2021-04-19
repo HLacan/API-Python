c851e54595d7176172e2d454aa7bb6ae4b9066dc
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-04-19 14:35:55 -0600

    se anadio el modelo de Doctor

---
+++ model/Doctor.py
@@ -0,0 +1,10 @@
+class Doctor():
+
+    def __init__(self, nombre, appelido, fecha, sexo, usuario, password, especialidad, telefono):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.fecha = fecha
+        self.sexo = sexo
+        self.usuario = usuario
+        self.password = password
+        self.especialidad = especialidad
