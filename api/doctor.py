4faf5bddd5ad1978af2081e5868d103f9aac2aea
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-04-19 16:42:05 -0600

    agregar rutas de doctor

--- api/doctor.py
+++ api/doctor.py
@@ -2,6 +2,18 @@ from flask import Blueprint
 
 urlDoctor = Blueprint('doctor', __name__,)
 
-@urlDoctor.route('/agregarDoctor')
+@urlDoctor.route('/addDoctor')
 def addDoctor():
     return 'Agregar Doctores'
+
+@urlDoctor.route('/getDoctores')
+def getDoctores():
+    return 'Obteniendo Doctores'
+
+@urlDoctor.route('/updateDoctor')
+def updateDoctor():
+    return 'Actualizando Doctor'
+
+@urlDoctor.route('/deleteDoctor')
+def deleteDoctor():
+    return 'Eliminando Doctor'
