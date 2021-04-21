5190d167a7fefbc0bd7af864ac359d279f03e697
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-04-21 14:56:53 -0600

    creacion del servidor

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 from flask import Flask
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+from flask import request
+from api.doctor import urlDoctor
+from api.login import urlLogin
 
-app = Flask(__name__)
+def create_app(): 
+    app = Flask(__name__)
+    app.register_blueprint(urlDoctor)
+    app.register_blueprint(urlLogin)
+    CORS(app) 
+    return app 
+
+app = create_app()
+
+@app.route('/')
+def index():
+    return 'index aqui estoy'
+
+#app.create_app()
+
+if __name__ == '__main__': 
+    app.run(debug=True) 
 
