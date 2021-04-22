e77ac7af90ad5499995105f76a2fcae21b4cbe74
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-04-21 18:05:10 -0600

    modificacion al metodo post de doctor

--- api/doctor.py
+++ api/doctor.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import jsonify
 from flask import request
+from flask_cors import CORS, cross_origin
 from model.Doctor import Doctor
 
 doctores = [Doctor("Herbert","Lacan","31/05/1999",'M',"hlacan","abc123","Huesos","22537520")]
@@ -11,12 +12,35 @@ urlDoctor = Blueprint('doctor', __name__,)
 
 @urlDoctor.route('/api/addDoctor', methods=['POST'])
 def addDoctor():
-    data = json.loads(request.data)
-    text = data.get("text", None)
-    if text is None:
-        return jsonify({"message":"text not found"})
-    else:
-        return jsonify(data)
+        repetido = True
+
+        json = request.get_json(force=True)
+
+        nombre = json['nombre']
+        apellido = json['apellido']
+        fecha = json['fecha']
+        sexo = json['sexo']
+        usuario = json['usuario']
+        contrasena = json['contrasena']
+        especialidad = json['especialidad']
+        telefono = json['telefono']
+
+        if nombre is None and apellido is None and fecha is None and sexo is None and usuario is None and contrasena is None and especialidad is None:
+            return jsonify({'res':'Campos Vacios'})    
+        else:
+            for i in doctores:
+                if i.usuario == usuario:
+                    repetido = False
+                    return jsonify({'res':'Usuario ya repetido'})
+                    break
+
+            if repetido == True:
+                if telefono is None:
+                        telefono = 'Ninguno'
+
+                doctores.append(Doctor(nombre, apellido, fecha, sexo, usuario, contrasena, especialidad, telefono))
+                return jsonify({'res':'Doctor agregado correctamente'})
+        
 
 @urlDoctor.route('/api/getDoctores' ,methods=['GET'])
 def getDoctores():
