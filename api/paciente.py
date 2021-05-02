c28c2e12f719e72c9bfbc68c12b9a554d51a3358
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-02 15:31:33 -0600

    metodos delete y update de pacientes

--- api/paciente.py
+++ api/paciente.py
@@ -6,6 +6,7 @@ from model.Paciente import Paciente
 urlPaciente = Blueprint('paciente', __name__,)
 
 pacientes = []
+pacientes.append(Paciente("pacnho","zapato","1","1","3","3","1"))
 
 @urlPaciente.route('/api/getPacientes', methods=['GET'])
 def getPacientes():
@@ -72,3 +73,46 @@ def getPaciente(usuario):
         return jsonify(res)
     else:
         return jsonify({'res':'No encontrado'})
+
+@urlPaciente.route('/api/updatePaciente', methods=['POST'])
+def updatePaciente():
+    json = request.get_json(force=True)
+
+    nombre = json['nombre']
+    apellido = json['apellido']
+    fecha = json['fecha']
+    genero = json['genero']
+    oldUsuario = json['oldUsuario']
+    newUsuario = json['newUsuario']
+    contrasena = json['contrasena']
+    telefono = json['telefono']
+
+    print(oldUsuario)
+    print(newUsuario)
+
+    for i in pacientes:
+        if i.usuario == oldUsuario:
+            i.nombre = nombre
+            i.apellido = apellido
+            i.fecha = fecha
+            i.genero = genero
+            i.usuario = newUsuario
+            i.contrasena = contrasena
+            i.telefono = telefono
+            return jsonify({'res':'modificado'})
+    
+    return jsonify({'res':'no ok'})
+
+@urlPaciente.route('/api/deletePaciente/<usuario>',methods=['GET'])
+def deleteEnfermera(usuario):
+    encontrado = False
+    for i in pacientes:
+        if i.usuario == usuario:
+            pacientes.remove(i)
+            encontrado = True
+            break
+        
+    if encontrado:
+        return jsonify({'res': 'eliminado'})
+    else:
+        return jsonify({'res':'usuario no encontrado'})
