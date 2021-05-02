31889c09d72ac38cfa19f13d039367c2bc7a29f6
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-02 15:32:06 -0600

    validacion de repeticion de usuarios

--- api/validate.py
+++ api/validate.py
@@ -1,12 +1,34 @@
 from flask import Blueprint
 from flask import jsonify
 from flask import request
+from api.admin import admin
 from api.doctor import doctores
 from api.enfermera import enfermeras
 from api.paciente import pacientes 
 
 urlValidate = Blueprint('validate', __name__,)
 
-@urlValidate.route('/api/validar', methods=['GET'])
-def validarUsuario():
-    json = json
+@urlValidate.route('/api/validar/<oldUsuario>/<newUsuario>', methods=['GET'])
+def validarUsuario(oldUsuario, newUsuario):
+    
+    for i in admin:
+        if newUsuario == i['usuario']:
+            return jsonify({'res':'existe'})
+            break
+    
+    for i in doctores:
+        if newUsuario == i.usuario:
+            return jsonify({'res':'existe'})
+            break
+
+    for i in enfermeras:
+        if newUsuario == i.usuario:
+            return jsonify({'res':'existe'})
+            break
+
+    for i in pacientes:
+        if newUsuario == i.usuario:
+            return jsonify({'res':'existe'})
+            break
+
+    return jsonify({'res':'libre'})
