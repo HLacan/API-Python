c5c6b9f5ec6cfc29e5dce9de0de2c72b9912bbe7
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-02 08:37:24 -0600

    metodo post de pacientes

--- api/paciente.py
+++ api/paciente.py
@@ -21,3 +21,31 @@ def getPacientes():
             'telefono': i.telefono
         })
     return jsonify(res)
+
+@urlPaciente.route('/api/addPaciente', methods=['POST'])
+def addPaciente():
+    repetido = True
+    json = request.get_json(force=True)
+    nombre = json['nombre']
+    apellido = json['apellido']
+    fecha = json['fecha']
+    genero = json['genero']
+    usuario = json['usuario']
+    contrasena = json['contrasena']
+    telefono = json['telefono']
+
+    if nombre is None and apellido is None and fecha is None and genero is None and usuario is None and contrasena is None:
+        return jsonify({'res': 'Campos Vacios'})
+    else:
+        for i in pacientes:
+            if i.usuario == usuario:
+                repetido = False
+                return jsonify({'res': 'Usuario ya repetido'})
+                break
+
+        if repetido == True:
+            if telefono is None:
+                telefono = 'Ninguno'
+
+            pacientes.append(Paciente(nombre, apellido, fecha, genero, usuario, contrasena, telefono))
+            return jsonify({'res': 'Paciente agregada correctamente'})
