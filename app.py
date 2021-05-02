effbcafd07dcd9a85c0af8d8d44b5b271c46f8af
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-02 08:40:20 -0600

    agregar rutas

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from flask_cors import CORS, cross_origin
 from flask import request
 from api.doctor import urlDoctor
 from api.login import urlLogin
+from api.enfermera import urlEnfermera
+from api.paciente import urlPaciente
+from api.validate import urlValidate
+from api.admin import urlAdmin
 
 def create_app(): 
     app = Flask(__name__)
     app.register_blueprint(urlDoctor)
     app.register_blueprint(urlLogin)
+    app.register_blueprint(urlEnfermera)
+    app.register_blueprint(urlPaciente)
+    app.register_blueprint(urlAdmin)
+    app.register_blueprint(urlValidate)
     CORS(app) 
     return app 
 
