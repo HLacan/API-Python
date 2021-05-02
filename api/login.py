d1f9eb8abc26744eb25fa6ac3837e03347215459
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-02 15:30:51 -0600

    validacion de login

--- api/login.py
+++ api/login.py
@@ -5,8 +5,11 @@ from flask_cors import CORS, cross_origin
 from flask import Blueprint
 from api.doctor import doctores
 from api.enfermera import enfermeras
+from api.admin import admin
+from api.paciente import pacientes
 
 urlLogin = Blueprint('login', __name__,)
+
 
 @urlLogin.route('/api/login', methods=['POST'])
 def login():
@@ -16,22 +19,62 @@ def login():
     usuario = json['usuario']
     contrasena = json['contrasena']
 
-    if usuario == 'admin' and contrasena == '1234':
-        return jsonify({'res':'admin'})
-    else:
-        for i in doctores:
-            if(usuario == i.usuario and contrasena == i.contrasena):
-                return jsonify({'res': 'doctor'})
-                break
+    for i in admin:
+        if(usuario == i['usuario'] and contrasena == i['contrasena']):
+            res = {
+                'nombre': i['nombre'],
+                'apellido': i['apellido'],
+                'usuario': i['usuario'],
+                'contrasena': i['contrasena'],
+                'tipo':'admin' 
+            }
+            return jsonify(res)
+            break
 
-        for i in enfermeras:
-            if(usuario == i.usuario and contrasena == i.contrasena):
-                return jsonify({'res': 'enfermera'})
-                break
+    for i in doctores:
+        if(usuario == i.usuario and contrasena == i.contrasena):
+            res = {
+                'nombre': i.nombre,
+                'apellido': i.apellido,
+                'fecha': i.fecha,
+                'genero': i.genero,
+                'usuario': i.usuario,
+                'contrasena': i.contrasena,
+                'especialidad': i.especialidad,
+                'telefono': i.telefono,
+                'tipo':'doctor'
+            }
+            return jsonify(res)
+            break
 
-        for i in pacientes:
-            if(usuario == i.usuario and contrasena == i.contrasena):
-                return jsonify({'res':'paciente'})
-                break
-    
-    return jsonify({'res':'no existe'})
+    for i in enfermeras:
+        if(usuario == i.usuario and contrasena == i.contrasena):
+            res = {
+                'nombre': i.nombre,
+                'apellido': i.apellido,
+                'fecha': i.fecha,
+                'genero': i.genero,
+                'usuario': i.usuario,
+                'contrasena': i.contrasena,
+                'telefono': i.telefono,
+                'tipo':'paciente'
+            }
+            return jsonify(res)
+            break
+
+    for i in pacientes:
+        if(usuario == i.usuario and contrasena == i.contrasena):
+            res = {
+                'nombre': i.nombre,
+                'apellido': i.apellido,
+                'fecha': i.fecha,
+                'genero': i.genero,
+                'usuario': i.usuario,
+                'contrasena': i.contrasena,
+                'telefono': i.telefono,
+                'tipo':'paciente'
+            }
+            return jsonify(res)
+            break
+
+    return jsonify({'res': 'no existe'})
