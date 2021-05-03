30038185e1b7dfb6db7c026b81ad0c576fd71901
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-03 00:55:04 -0600

    listado de citas y compras

--- model/Paciente.py
+++ model/Paciente.py
@@ -1,5 +1,5 @@
 class Paciente():
-    def __init__(self, nombre, apellido, fecha, genero, usuario, contrasena, telefono):
+    def __init__(self, nombre, apellido, fecha, genero, usuario, contrasena, telefono, cita = None, compras = None):
         self.nombre = nombre
         self.apellido = apellido
         self.fecha = fecha
@@ -7,3 +7,9 @@ class Paciente():
         self.usuario = usuario
         self.contrasena = contrasena
         self.telefono = telefono
+        if cita is None:
+            cita = []
+        self.cita = cita
+        if compras is None:
+            compras = []
+        self.compras = compras
