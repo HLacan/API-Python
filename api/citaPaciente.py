8eb9d97ba1e50be7515644c5d3e70943f0b6a201
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-03 00:51:36 -0600

    manejo de citas

--- api/citaPaciente.py
+++ api/citaPaciente.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask import request
 from api.paciente import pacientes
 from model.CitaPaciente import CitaPaciente
+from api.doctor import doctores
 
 urlCitaPaciente = Blueprint('citaPaciente', __name__,)
 
@@ -16,20 +17,31 @@ def addCitaPaciente():
     usuario = json['usuario']
     estado = json['estado']
 
-    print(fecha)
-    print(hora)
-    print(descripcion)
-    print(estado)
+    #print(len(pacientes[0].cita))
+    #print(len(pacientes[1].cita))
+
+    #for i in range(len(pacientes)):
+        #print(len(pacientes[i].cita))  
+
+    
 
     for i in pacientes:
         if i.usuario == usuario:
-            i.cita.append(CitaPaciente(fecha,hora,descripcion, estado))
-            return jsonify({'res':'agregado'})
-
+            if len(i.cita) == 0:
+                i.cita.append(CitaPaciente(fecha,hora,descripcion, estado, usuario))
+                return jsonify({'res':'agregado'})
+            else:
+                for j in i.cita:
+                    if j.estado == 'pendiente' or j.estado == 'aceptado':
+                        return jsonify({'res':'ya hay citas'})
+                    else:
+                        i.cita.append(CitaPaciente(fecha,hora,descripcion, estado,  usuario))
+                        return jsonify({'res':'agregado'})
     return jsonify({'res':'nel'})
-
+    
 @urlCitaPaciente.route('/api/getCitasPaciente/<usuario>', methods=['GET'])
 def getCitasPaciente(usuario):
+    encontrado = False
     response = []
     for i in pacientes:
         if i.usuario == usuario:
@@ -38,7 +50,89 @@ def getCitasPaciente(usuario):
                     'fecha': j.fecha,
                     'hora': j.hora,
                     'descripcion': j.descripcion,
-                    'estado': j.estado
+                    'estado': j.estado,
+                    'usuario':  j.usuario
                 })
+    return jsonify(response)
+
+@urlCitaPaciente.route('/api/getCitasPendientes', methods=['GET'])
+def getCitasPendientes():
+    response = []
+    for i in pacientes:
+            for j in i.cita:
+                if j.estado == 'pendiente':
+                    response.append({
+                        'fecha': j.fecha,
+                        'hora': j.hora,
+                        'descripcion': j.descripcion,
+                        'estado': j.estado,
+                        'usuario': j.usuario
+                    })
+    return jsonify(response)
+
+@urlCitaPaciente.route('/api/updateEstado', methods=['POST'])
+def updateEstado():
+    json = request.get_json(force=True)
+
+    estado = json['estado']
+    usuario = json['usuario']
+    doctor = json['doctor']
+
+    if estado == 'rechazado':
+        for l in pacientes:
+            if l.usuario == usuario:
+                for j in l.cita:
+                    if j.estado == 'pendiente':
+                        j.estado = 'rechazado'
+                        return jsonify({'res':'rechazado'})
+                        break
+    else:
+        for i in doctores:
+            if i.usuario == doctor:
+                for j in pacientes:
+                    if j.usuario == usuario:
+                        for k in j.cita:
+                            if k.estado == 'pendiente':
+                                k.estado = 'aceptado'
+                                i.cita.append(CitaPaciente(k.fecha, k.hora, k.descripcion, k.estado, k.usuario))
+                                return({'res':'agregado'})
+
+
+
+    return jsonify({'res':'modificado'})
+
+@urlCitaPaciente.route('/api/getCitasAceptadas/<usuario>', methods=['GET'])
+def getCitasAceptadas(usuario):
+    response = []
+ 
+    for i in doctores:
+        if i.usuario == usuario:
+            for j in i.cita:
+                if j.estado == 'aceptado':
+                    response.append({
+                        'fecha': j.fecha,
+                        'hora': j.hora,
+                        'descripcion': j.descripcion,
+                        'estado': j.estado,
+                        'usuario': j.usuario
+                    })
+
+    return jsonify(response)
+
+@urlCitaPaciente.route('/api/getCitasAceptadas', methods=['GET'])
+def getCitasAceptadasE():
+    response = []
+ 
+    for i in doctores:
+            for j in i.cita:
+                if j.estado == 'aceptado':
+                    response.append({
+                        'fecha': j.fecha,
+                        'hora': j.hora,
+                        'descripcion': j.descripcion,
+                        'estado': j.estado,
+                        'usuario': j.usuario
+                    })
+
     return jsonify(response)
 
