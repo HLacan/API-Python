f73b546b653286135a4017429c80d4bcb9bba8d6
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-03 00:53:27 -0600

    modelo de cita

---
+++ model/CitaPaciente.py
@@ -0,0 +1,7 @@
+class CitaPaciente():
+    def __init__(self, fecha, hora, descripcion, estado, usuario):
+        self.fecha = fecha
+        self.hora = hora
+        self.descripcion = descripcion
+        self.estado = estado
+        self.usuario = usuario
