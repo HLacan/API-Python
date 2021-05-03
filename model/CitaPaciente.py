6a51cd632e3411a495faca2b97b0b0dc9ab17665
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-03 10:03:29 -0600

    campo de doctor

--- model/CitaPaciente.py
+++ model/CitaPaciente.py
@@ -1,7 +1,8 @@
 class CitaPaciente():
-    def __init__(self, fecha, hora, descripcion, estado, usuario):
+    def __init__(self, fecha, hora, descripcion, estado, usuario, doctor = None):
         self.fecha = fecha
         self.hora = hora
         self.descripcion = descripcion
         self.estado = estado
         self.usuario = usuario
+        self.doctor = doctor
