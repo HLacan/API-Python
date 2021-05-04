8ce4e304e53c5dc56e5b0e93ca0e926f2945e336
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-03 19:34:00 -0600

    modificar admin

--- api/admin.py
+++ api/admin.py
@@ -10,3 +10,23 @@ urlAdmin = Blueprint('admin', __name__,)
 @urlAdmin.route('/api/getAdmin', methods=['GET'])
 def getAdmin():
     return jsonify(admin)
+
+@urlAdmin.route('/api/updateAdmin', methods=['POST'])
+def updateAdmin():
+    json = request.get_json(force=True)
+
+    nombre = json['nombre']
+    apellido = json['apellido']
+    oldUsuario = json['oldUsuario']
+    newUsuario = json['newUsuario']
+    contrasena = json['contrasena']
+
+    for i in admin:
+        if i['usuario'] == oldUsuario:
+            i['nombre'] = nombre
+            i['apellido'] = apellido
+            i['usuario'] = newUsuario
+            i['contrasena'] = contrasena
+            return jsonify({'res':'modificado'})
+
+    return jsonify({'res':'error'})
