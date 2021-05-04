57d066bbc1dff66b01532336ad1f72c4fe3ce254
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-03 19:32:44 -0600

    validar nombre de medicamento

--- api/validate.py
+++ api/validate.py
@@ -5,12 +5,13 @@ from api.admin import admin
 from api.doctor import doctores
 from api.enfermera import enfermeras
 from api.paciente import pacientes 
+from api.medicamento import medicamentos
 
 urlValidate = Blueprint('validate', __name__,)
 
 @urlValidate.route('/api/validar/<oldUsuario>/<newUsuario>', methods=['GET'])
 def validarUsuario(oldUsuario, newUsuario):
-    
+
     for i in admin:
         if newUsuario == i['usuario']:
             return jsonify({'res':'existe'})
@@ -28,6 +29,16 @@ def validarUsuario(oldUsuario, newUsuario):
 
     for i in pacientes:
         if newUsuario == i.usuario:
+            return jsonify({'res':'existe'})
+            break
+
+    return jsonify({'res':'libre'})
+
+@urlValidate.route('/api/validarM/<oldNombre>/<newNombre>', methods=['GET'])
+def validarMedicamento(oldNombre, newNombre):
+    
+    for i in medicamentos:
+        if newNombre == i.nombre:
             return jsonify({'res':'existe'})
             break
 
